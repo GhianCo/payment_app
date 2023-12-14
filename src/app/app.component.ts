b59982ec01ac95ec1e7780020685390da93c21ed
GhianCo <ghiancorx@gmail.com>
2023-12-13 23:30:51 -0500

    primera session angular 17

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,4 +11,22 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'paymentApp';
+
+  //Objeto persona
+  
+  persona = {
+    persona_id: 1,
+    persona_nombre: "Pedro"
+  }
+
+  persona1 = {
+    id: 2,
+    nombre: "Miguel"
+  }
+
+  reporte = {
+    persona_nombre: 'Persona',
+    empresa_nombre: "empresa"
+  }
+
 }
