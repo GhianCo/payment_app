5c44af1da9726a70df67675138353b4ebc014239
GhianCo <ghiancorx@gmail.com>
2023-12-19 23:44:29 -0500

    avance 1 tailwind

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -9,24 +10,22 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
-  title = 'paymentApp';
 
-  //Objeto persona
-  
-  persona = {
-    persona_id: 1,
-    persona_nombre: "Pedro"
+export class AppComponent implements  OnInit, OnDestroy {
+
+  public lista: any[] = [];
+
+  constructor(private http: HttpClient){}
+
+  //Inicializador
+  ngOnInit(): void {
+    this.http.get('http://localhost/tiendasmilan/api/rest/producto/obtenerListaProductoPaginadoPorLocal/1/-1/-1/-1/-1/1/10?token=MjM0MjMyMw')
+      .subscribe((data: any) => {
+        this.lista = data?.data;
+      });
   }
 
-  persona1 = {
-    id: 2,
-    nombre: "Miguel"
+  ngOnDestroy(): void {
+    console.log('destroy');
   }
-
-  reporte = {
-    persona_nombre: 'Persona',
-    empresa_nombre: "empresa"
-  }
-
 }
